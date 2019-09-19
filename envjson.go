d9615d97c02332c257b0d18efdf658f2ec972123
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2019-09-19 22:47:10 +0900

    fix

--- envjson.go
+++ envjson.go
@@ -1,1 +1,67 @@
 package envjson
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const DefaultFileName = ".envjson"
+
+func Load(filenames ...string) error {
+	filenames = filenamesOrDefault(filenames...)
+
+	for _, filename := range filenames {
+		if err := load(filename); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func load(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var m map[string]interface{}
+	if err := json.NewDecoder(f).Decode(&m); err != nil {
+		return err
+	}
+
+	if err := setEnv(m); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setEnv(m map[string]interface{}) error {
+	for key, val := range m {
+		if str, ok := val.(string); ok {
+			if err := os.Setenv(key, str); err != nil {
+				return err
+			}
+			continue
+		}
+
+		b, err := json.Marshal(val)
+		if err != nil {
+			return err
+		}
+
+		if err := os.Setenv(key, string(b)); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func filenamesOrDefault(filenames ...string) []string {
+	if len(filenames) == 0 {
+		return []string{DefaultFileName}
+	}
+	return filenames
+}
--- envjson_test.go
+++ envjson_test.go
@@ -1,1 +1,58 @@
 package envjson
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+
+}
+
+func TestSetEnv(t *testing.T) {
+	input := map[string]interface{}{
+		"debug": true,
+		"db": map[string]string{
+			"user":   "user_1",
+			"pass":   "pass_1",
+			"dbname": "dbname_1",
+		},
+	}
+
+	if err := setEnv(input); err != nil {
+		t.Fatalf("setEnv is err: %v", err)
+	}
+
+	if got, want := os.Getenv("debug"), "true"; want != got {
+		t.Errorf("env debug is wrong, want: %v, but got: %v", want, got)
+	}
+
+	if got, want := os.Getenv("db"), `{"dbname":"dbname_1","pass":"pass_1","user":"user_1"}`; want != got {
+		t.Errorf("env db is wrong, want: %v, but got: %v", want, got)
+	}
+}
+
+func TestFilenamesOrDefault(t *testing.T) {
+	tests := []struct {
+		input []string
+		want  []string
+	}{
+		{
+			input: []string{},
+			want:  []string{".envjson"},
+		},
+		{
+			input: []string{"input.json"},
+			want:  []string{"input.json"},
+		},
+	}
+
+	for i, test := range tests {
+		got := filenamesOrDefault(test.input...)
+
+		if ok := reflect.DeepEqual(got, test.want); !ok {
+			t.Errorf("tests[%d] - filenamesOrDefault is wrong, want: %v, but got: %v", i, test.want, got)
+		}
+	}
+}
